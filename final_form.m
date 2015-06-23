4bc9f6828ac9109f5dafa04555ac21f1b1ce54c7
linxichen <linxichen88@gmail.com>
2015-06-23 14:58:04 -0400

    Failing version

--- final_form.m
+++ final_form.m
@@ -10,13 +10,21 @@
 % 3. General eqm, CARA
 % 4. Uses steady state calibration file
 
+% 05/28/2015: add tightness ratio and matching probability
+% ttheta = 1/(measure of firms who do investment)
+% mmu = aalpha0*ttheta^aalpha1;
+% I let ttheta to depend on price q. This will be reflected in optimization of q
+% In KS:
+% ttheta = pphi_theta*log_aggstate+pphi_thetaq*q
+
 %% Housekeeping
 clear; close all; clc;
 deep_para_quarterly;
+MC = 0; % let it be small, so that C is always positive
 
 %% Accuracy control
 nk = 50; % number of grid points on capital stock
-nfine = 150; % number of grid points for policy functions and simulation
+nfine = 50; % number of grid points for policy functions and simulation
 nx = 7; % number of grid points on idiosyncractic prod.
 nz = 5; % number of grid points on aggregate productivity
 ns = nx*nz*2; % total dimension of exo state, (idio, agg, ssigmax)
@@ -26,7 +34,7 @@ m = 3; % support is m s.d. away from mean
 tol = 1e-3; % when to stop VFI and KS
 outer_tol = 1e-3;
 maxiter = 100;
-damp = 0.3; % help with outer convergence
+damp = 0.5; % help with outer convergence
 
 %% Grids
 [Z,PZ] = tauchen(nz,0,rrhoz,ssigmaz,m); Z = exp(Z); % agg TFP
@@ -57,8 +65,8 @@ end
 % The tail index has to be larger than 1/(1-aalpha) = 1.4286, this implies
 % q = 1.05.
 %=========================================================================%
-qmin = 1.00001;
-qmax = 2;
+qmin = 0.00001;
+qmax = 1.1;
 q_grid = linspace(qmin,qmax,nq); 
 % Capital grid
 min_k = 1;
@@ -68,7 +76,8 @@ for i_k = 1:nk
     k_grid(i_k) = max_k*(1-ddelta)^(nk-i_k);
 end
 % k_grid = linspace(min_k,max_k,nk)';
-fine_grid = linspace(k_grid(1),k_grid(nk),nfine)';
+% fine_grid = linspace(k_grid(1),k_grid(nk),nfine)';
+fine_grid = k_grid;
 noinvest_ind = ones(nk,1); % for each k, the index of tmr k if no invest
 for i_k = 1:nk
     [~,noinvest_ind(i_k)] = min(abs(k_grid-(1-ddelta)*k_grid(i_k)));
@@ -93,24 +102,30 @@ K_grid = linspace(k_grid(1),k_grid(nk),nK)'; % Aggregate capital grid
 
 %% Aggregate rules and its parameters
 % Rule of q as function of aggregate states
-pphi_qC = log(1.1); % constant term
+pphi_qC = log(mean(q_grid)); % constant term
 pphi_qK = 0; % w.r.t agg K
 pphi_qz = 0; % w.r.t agg TFP
 pphi_qssigmax = 0; % w.r.t uncertainty
 pphi_q = [pphi_qC,pphi_qK,pphi_qssigmax,pphi_qz];
 
 pphi_KK = 9.870171e-01; 
-pphi_KC = log(10); 
+pphi_KC = log(mean(k_grid)); 
 pphi_Kz = 0;
 pphi_Kssigmax = 0;% Aggregate Law of motion for aggregate capital
 pphi_K = [pphi_KC,pphi_KK,pphi_Kssigmax,pphi_Kz];
 
-pphi_CC = log(1.3);
+pphi_CC = log(0.3); % consumption
 pphi_CK = 0.0;
 pphi_Cz = 0.0;
 pphi_Cssigmax = 0.0;
 pphi_C = [pphi_CC,pphi_CK,pphi_Cz,pphi_Cssigmax];
 
+pphi_tthetaC = log(1); % tightness ratio depends on q
+pphi_tthetaK = 0.0;
+pphi_tthetaz = 0.0;
+pphi_tthetassigmax = 0.0;
+pphi_ttheta = [pphi_tthetaC,pphi_tthetaK,pphi_tthetaz,pphi_tthetassigmax];
+pphi_tthetaq = 0.1;% lower q -- more firms invest -- lower ttheta
 if (exist('aggrules.mat','file') == 2)
     load aggrules.mat;
 end
@@ -149,7 +164,7 @@ W_new_fine = zeros(nfine,ns,nK,nq);
 U_new_fine = zeros(nfine,ns,nK);
 koptind_active = zeros(nk,ns,nK,nq);
 koptind = zeros(nk,ns,nK,nq);
-kopt_fine = zeros(nfine,ns,nK,nq);
+kopt_active_fine = zeros(nfine,ns,nK,nq);
 active = zeros(nk,ns,nK,nq);
 
 % Prepare for Simulation stuff
@@ -160,6 +175,8 @@ Ksim = kss*ones(1,T);
 qsim = mean(q_grid)*ones(1,T);
 revenue = zeros(nq,1); % revenue of inv producer
 demand = zeros(nq,T);
+mmusim = zeros(1,T); % finding rate under optimal q
+tthetasim = zeros(1,T); % tightness ratio under optimal q
 dist_k = zeros(nfine,nx,T); % !! Should change this to Pareto
 dist_k(ceil(nfine/2),:,:) = ones(nx,T)/nx; 
 
@@ -205,7 +222,8 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
                 [~,i_Kplus] = min(abs(K_grid-exp(pphi_K*log_aggstate)));
                 [~,i_qplus] = min(abs(q_grid-exp(pphi_q*log_aggstate)));
                 C = exp(pphi_C*log_aggstate);
-                %=========Take as given wage,find profit in c units=======%
+              
+                %===============Take as given wage,find profit============%
                 w = ppsi_n*C;
                 L = (w*k_grid.^(-aalpha)/Z(i_z)/X(i_x)/v).^(1/(v-1));
                 profit = Z(i_z)*X(i_x)*k_grid.^aalpha.*L.^v - w.*L;
@@ -226,8 +244,13 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
                 % Not optimally! We need to find maximizing kplus
                 %=========================================================%
                 for i_q = 1:nq
-                    netprofit = repmat(profit,1,nk)-q_grid(i_q)*(inv_mat).*(pos_inv+neg_inv*pphi);
-                    candidate = llambda*netprofit  + bbeta*repmat(EV_invest',nk,1);
+                    ttheta = exp(pphi_ttheta*log_aggstate+pphi_tthetaq*log(q_grid(i_q))); % tightness for this q
+                   % mmu = 1./(1+tttheta_old.^(-aalpha0)).^(1/aalpha0);
+                    aalpha0 = 0.99;aalpha1 = .0; % let mmu always close to 1
+                    mmu = aalpha0*ttheta^aalpha1;
+                    % mmu = 1./((1+ttheta.^(aalpha0)).^(1/aalpha0));
+                    netprofit = repmat(profit,1,nk)-mmu*q_grid(i_q)*(inv_mat).*(pos_inv+neg_inv*pphi);
+                    candidate = llambda*netprofit  + bbeta*(mmu*repmat(EV_invest',nk,1)+(1-mmu)*repmat(EV_noinvest,1,nk));
                     %=====================================================%
                     % candidate(i_k,i_kplus) is the thing inside max
                     % operator. expected value tomorrow depends only on
@@ -236,6 +259,7 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
                     % function so it is pre-computed outside for loops
                     %=====================================================%
                     [W_new(:,i_s,i_K,i_q),koptind_active(:,i_s,i_K,i_q)] = max(candidate,[],2);
+                    % W_new(:,i_s,i_K,i_q) = mmu*W_temp+(1-mmu)*U_new(:,i_s,i_K); % match with prob. mmu 
                     V_new(:,i_s,i_K,i_q) = max(W_new(:,i_s,i_K,i_q),U_new(:,i_s,i_K));
                 end
             end
@@ -255,8 +279,9 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
     
     active = W_new > repmat(U_new,1,1,1,nq);
     koptind = repmat(noinvest_ind,1,ns,nK,nq).*(1-active) + active.*koptind_active;
+    kopt_active = k_grid(koptind_active);
     kopt = k_grid(koptind);
-    plot(k_grid,kopt(:,sub2ind([nx nz 2],ceil(nx/2),ceil(nz/2),1),ceil(nK/2),ceil(nq/2))-(1-ddelta)*k_grid)
+    plot(k_grid,kopt(:,sub2ind([nx nz 2],ceil(nx/2),ceil(nz/2),2),ceil(nK/2),5)-(1-ddelta)*k_grid)
     save('valuefunctions.mat','V_new','W_new','U_new','V_old','W_old','U_old')
     
     % Interpolate on finer grid
@@ -265,12 +290,14 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
             for i_s = 1:ns
                 U_new_fine(:,i_s,i_K) = interp1(k_grid,U_new(:,i_s,i_K),fine_grid,'linear')';
                 W_new_fine(:,i_s,i_K,i_q) = interp1(k_grid,W_new(:,i_s,i_K,i_q),fine_grid,'linear')';
-                kopt_fine(:,i_s,i_K,i_q) = interp1(k_grid,kopt(:,i_s,i_K,i_q),fine_grid,'linear')';
+                kopt_active_fine(:,i_s,i_K,i_q) = interp1(k_grid,kopt_active(:,i_s,i_K,i_q),fine_grid,'linear')';
             end
         end
     end    
     active_fine = W_new_fine > repmat(U_new_fine,1,1,1,nq);
-    % plot(fine_grid,kopt_fine(:,sub2ind([nx nz 2],ceil(nx/2),ceil(nz/2),1),ceil(nK/2),ceil(nq/2))-(1-ddelta)*fine_grid)
+    kopt_fine = (1-ddelta)*repmat(fine_grid,1,ns,nK,nq).*(1-active_fine) + active_fine.*kopt_active_fine;
+    plot(fine_grid,kopt_fine(:,sub2ind([nx nz 2],ceil(nx/2),ceil(nz/2),2),ceil(nK/2),1)-(1-ddelta)*fine_grid)
+
     
     %% Given individual policies, simulate a large panel to update aggregate law of motion
     % Zero out after each outer iteration.
@@ -297,12 +324,16 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
         log_aggstate = [1; log(Ksim(t)); log(ssigmax(ssigmaxsim(t))); log(zsim(t))];
         C = exp(pphi_C*log_aggstate);
         w = ppsi_n*C;
-        
        
         % According to policy functions, find the optimal q
+        % notice that investment goods producer understand that q can
+        % affect ttheta
         for i_q = 1:nq
-            tot_profit_grid = (q_grid(i_q)-w)*dist_k(:,:,t).*(kopt_fine(:,whichs,i_K,i_q)-(1-ddelta)*repmat(fine_grid,1,nx)).*(active_fine(:,whichs,i_K,i_q));
-            demand_grid = dist_k(:,:,t).*(kopt_fine(:,whichs,i_K,i_q)-(1-ddelta)*repmat(fine_grid,1,nx)).*(active_fine(:,whichs,i_K,i_q));
+            ttheta_temp = exp(pphi_ttheta*log_aggstate+pphi_tthetaq*q_grid(i_q));% tightness ratio given q and states
+            % mmu_temp = 1./((1+ttheta_temp.^(aalpha0)).^(1/aalpha0));% matching rate for final goods producers
+            mmu_temp = aalpha0*ttheta_temp^aalpha1;
+            tot_profit_grid = mmu_temp*(q_grid(i_q)-w)*dist_k(:,:,t).*(kopt_active_fine(:,whichs,i_K,i_q)-(1-ddelta)*repmat(fine_grid,1,nx)).*(active_fine(:,whichs,i_K,i_q));
+            demand_grid = mmu_temp*dist_k(:,:,t).*(kopt_active_fine(:,whichs,i_K,i_q)-(1-ddelta)*repmat(fine_grid,1,nx)).*(active_fine(:,whichs,i_K,i_q));
             revenue(i_q,t) = sum(tot_profit_grid(:));
             demand(i_q,t) = sum(demand_grid(:));
         end
@@ -312,6 +343,9 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
         % Evolution under the argmax q
         if (t<=T-1)
             output = 0;
+            ttheta_temp = exp(pphi_ttheta*log_aggstate+pphi_tthetaq*q_grid(i_qmax));% tightness ratio given q and states
+            mmu_temp = aalpha0*ttheta_temp^aalpha1;
+            % mmu_temp = 1./((1+ttheta_temp.^(aalpha0)).^(1/aalpha0));% matching rate for final goods producers
             for i_k = 1:nfine
                 for i_x = 1:nx
                     %======GE: Find output on each state==================%
@@ -325,24 +359,34 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
                     
                     % Assign mass to tomorrow's distribution
                     for i_xplus = 1:nx
-                        if (kplus>fine_grid(1) && kplus<fine_grid(nfine))
-                            lower_ind = find(fine_grid<=kplus,1,'last');
-                            upper_ind = lower_ind + 1;
-                            denom = fine_grid(upper_ind)-fine_grid(lower_ind);
-                            dist_k(lower_ind,i_xplus,t+1) = dist_k(lower_ind,i_xplus,t+1) + whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t)*(kplus-fine_grid(lower_ind))/denom;
-                            dist_k(upper_ind,i_xplus,t+1) = dist_k(upper_ind,i_xplus,t+1) + whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t)*(fine_grid(upper_ind)-kplus)/denom;
-                        elseif (kplus<=fine_grid(1))
-                            dist_k(1,i_xplus,t+1) = dist_k(1,i_xplus,t+1) + whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
-                        elseif (kplus>=fine_grid(nfine))
-                            dist_k(nfine,i_xplus,t+1) = dist_k(nfine,i_xplus,t+1) + whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
+                        if active_fine(i_k,i_x) == 1
+                            if (kplus>fine_grid(1) && kplus<fine_grid(nfine))
+                                lower_ind = find(fine_grid<=kplus,1,'last');
+                                upper_ind = lower_ind + 1;
+                                denom = fine_grid(upper_ind)-fine_grid(lower_ind);
+                                dist_k(lower_ind,i_xplus,t+1) = dist_k(lower_ind,i_xplus,t+1) + mmu_temp*whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t)*(kplus-fine_grid(lower_ind))/denom;
+                                dist_k(upper_ind,i_xplus,t+1) = dist_k(upper_ind,i_xplus,t+1) + mmu_temp*whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t)*(fine_grid(upper_ind)-kplus)/denom;
+                                dist_k(noinvest_ind_fine(i_k),i_xplus,t+1) = dist_k(noinvest_ind_fine(i_k),i_xplus,t+1)+(1-mmu_temp)*whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
+                            elseif (kplus<=fine_grid(1))
+                                dist_k(1,i_xplus,t+1) = dist_k(1,i_xplus,t+1) + mmu_temp*whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
+                                dist_k(noinvest_ind_fine(i_k),i_xplus,t+1) = dist_k(noinvest_ind_fine(i_k),i_xplus,t+1)+(1-mmu_temp)*whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
+                            elseif (kplus>=fine_grid(nfine))
+                                dist_k(nfine,i_xplus,t+1) = dist_k(nfine,i_xplus,t+1) + mmu_temp*whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
+                                dist_k(noinvest_ind_fine(i_k),i_xplus,t+1) = dist_k(noinvest_ind_fine(i_k),i_xplus,t+1)+(1-mmu_temp)*whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
+                            end
+                        else
+                            dist_k(noinvest_ind_fine(i_k),i_xplus,t+1) = dist_k(noinvest_ind_fine(i_k),i_xplus,t+1)+whichprob(i_x,i_xplus)*dist_k(i_k,i_x,t);
                         end
                     end  
                 end
             end
-            
-            % Eventually find implied consumption
-            Csim(t) = output;
         end
+            
+        % Eventually find implied consumption
+        Csim(t) = output - MC*demand(i_qmax,t);
+        % also find implied tightness ratio
+        measure_active = sum(sum(dist_k(:,:,t).*active_fine(:,whichs,i_K,i_qmax)));% how many firms choose to search
+        tthetasim(t) = 1/measure_active;
     end
     
     % update kss
@@ -373,13 +417,26 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
     Rsq_C = 1-(e'*e)/(ytilde'*ytilde);
     pphi_C_new = damp*bbeta_C' + (1-damp)*pphi_C;
     
+    % Regress to get ttheta law
+    Y = log(tthetasim(1+burnin:T-1))';
+    XX1 = [XX,log(qsim(1+burnin:T-1))'];
+    bbeta_ttheta = (XX1'*XX1)\(XX1'*Y);
+    e = Y-XX1*bbeta_ttheta;
+    ytilde = Y-mean(Y);
+    Rsq_ttheta = 1-(e'*e)/(ytilde'*ytilde);
+    pphi_ttheta_new = damp*bbeta_ttheta(1:end-1)' + (1-damp)*pphi_ttheta;
+    pphi_tthetaq_new = damp*bbeta_ttheta(end) + (1-damp)*pphi_tthetaq;
+    
     % Update mmu_old as well
-    outer_diff = norm([pphi_K,pphi_q,pphi_C]-[pphi_K_new,pphi_q_new,pphi_C_new],Inf);
+    outer_diff = norm([pphi_K,pphi_q,pphi_C,pphi_ttheta,pphi_tthetaq]-[pphi_K_new,pphi_q_new,pphi_C_new,pphi_ttheta_new,pphi_tthetaq_new],Inf);
     
     % Update mmu_old as well
     pphi_K = pphi_K_new;
     pphi_q = pphi_q_new;
     pphi_C = pphi_C_new;
+    pphi_ttheta = pphi_ttheta_new;
+    pphi_tthetaq = pphi_tthetaq_new;
+    
    
     %% Plot something
 %     i_x = 4;
@@ -398,7 +455,7 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
     
     %%
     outer_iter = outer_iter + 1;
-    disp_text = sprintf('Rsq_K = %d, Rsq_q = %d, Rsq_C = %d',Rsq_K,Rsq_q,Rsq_C);
+    disp_text = sprintf('Rsq_K = %d, Rsq_q = %d,Rsq_C = %d,Rsq_ttheta = %d',Rsq_K,Rsq_q,Rsq_C,Rsq_ttheta);
     disp(disp_text);
     disp_text = sprintf('log(q) = %d + %d * log(K) + %d * log(ssigmax)+%d * log(z)',pphi_q);
     disp(disp_text);
@@ -406,11 +463,13 @@ while ((outer_diff > outer_tol) && (outer_iter < maxiter))
     disp(disp_text);
     disp_text = sprintf('log(C) = %d + %d * log(K) + %d * log(ssigmax)+%d * log(z)',pphi_C);
     disp(disp_text);
+    disp_text = sprintf('log(ttheta) = %d + %d * log(K) + %d * log(ssigmax)+%d * log(z)+%d*log(q)',pphi_C,pphi_tthetaq);
+    disp(disp_text);
     disp_text = sprintf('KS Iter = %d, KS err = %d, Current VFI Iter = %d, err = %d',outer_iter,outer_diff,iter,err);
     disp(disp_text);
     disp('===============================');
-    save('aggrules.mat','pphi_K','pphi_q','pphi_C');
-    save('Rsq.mat','Rsq_K','Rsq_q','Rsq_C');
+    save('aggrules.mat','pphi_K','pphi_q','pphi_C','pphi_ttheta');
+    save('Rsq.mat','Rsq_K','Rsq_q','Rsq_C','Rsq_ttheta');
     
 end
 
